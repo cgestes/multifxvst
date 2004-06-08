3d528d2319a555bb0609f6d126767de80297073a
CTAF <CTAF@3e78e570-a0aa-6544-9a6b-7e87a0c009bc>
2004-06-08 22:56:02 +0000

    Fichier/répertoire effacé
    git-svn-id: file:///home/ctaf42/Downloads/tmp/MultifxVST@7 3e78e570-a0aa-6544-9a6b-7e87a0c009bc

--- vstplugins/make paquetage.bat
+++
@@ -1,5 +0,0 @@
-REM SCRIPT EXECUTER AVANT PAQUETAGE
-copy /Y "ControlsGUI.dll" "gratomatic\ControlGUI.dll_dyn"
-copy /Y "ControlsGUI stat.dll" "gratomatic\ControlGUI.dll_stat"
-del /Q /F "gratomatic\ControlGUI.dll"
-pause
